a7a89c0dbdb8a722becd2e2157e8bb13d5df49f0
Anuradhe007 <anu.bab007@gmail.com>
2017-11-27 18:35:02 +0530

    Initial Commit.

---
+++ src/bankupdated/BankUpdated.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankupdated;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.DialogPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Prabhath
+ */
+public class BankUpdated extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setAlwaysOnTop(true);
+        
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+ 
+        //set Stage boundaries to the lower right corner of the visible bounds of the main screen
+        stage.setX(primaryScreenBounds.getMinX() + primaryScreenBounds.getWidth() - 300);
+        stage.setY(0);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
