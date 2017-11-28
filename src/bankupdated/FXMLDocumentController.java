5e92d34dc82750200c7734b6fb0b04630359ec99
Anuradhe007 <anu.bab007@gmail.com>
2017-11-28 16:44:17 +0530

    Implemented Frame2 UI.

--- src/bankupdated/FXMLDocumentController.java
+++ src/bankupdated/FXMLDocumentController.java
@@ -5,12 +5,22 @@
  */
 package bankupdated;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,11 +30,31 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private JFXButton jfxButton;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        try{
+             Stage stage1 = (Stage) jfxButton.getScene().getWindow();
+             stage1.hide();
+             
+             Parent root = FXMLLoader.load(getClass().getResource("Frame2.fxml"));
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setAlwaysOnTop(true);
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        //set Stage boundaries to the lower right corner of the visible bounds of the main screen
+        stage.setX(primaryScreenBounds.getMinX() + primaryScreenBounds.getWidth() - 600);
+        stage.setY(0);
+        stage.show();
+          } catch(Exception e) {
+              e.printStackTrace();
+          }
     }
     
     @Override
